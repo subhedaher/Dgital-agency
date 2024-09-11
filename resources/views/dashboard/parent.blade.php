ec6c1c8b407cf863846808013f2398e595dfee25
subhe daher <subhedaher@gmail.com>
2024-09-12 01:48:53 +0300

    Edit Name Dashboard

--- resources/views/dashboard/parent.blade.php
+++ resources/views/dashboard/parent.blade.php
@@ -132,7 +132,7 @@
                   </g>
                 </svg>
               </span>
-              <span class="app-brand-text demo menu-text fw-bolder ms-2">Dashboard</span>
+              <span class="app-brand-text demo menu-text fw-bolder ms-2">{{ Config::get('app.name') }}</span>
             </a>
 
             <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
